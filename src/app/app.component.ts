c2ee59a971fc42b3c897689e5edce7c88f6ab96a
John Siu <john.sd.siu@gmail.com>
2019-09-06 18:19:36 -0400

    Add newTimerHR and newTimerCD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ import { SimpleTimer } from 'ng2-simple-timer';
 	selector: 'app-root',
 	template: `
 		<h3>{{title}}</h3>
-		<div><button (click)="subscribeTimer0()">{{timer0button}}  1 sec timer</button>{{counter0}}</div>
-		<div><button (click)="subscribeTimer1()">{{timer1button}}  5 sec timer</button>{{counter1}}</div>
-		<div><button (click)="subscribeTimer2()">{{timer2button}} 10 sec timer</button>{{counter2}}</div>
+		<div><button (click)="subscribeTimer0()">{{timer0button}} {{timer0Name}} timer</button>{{counter0}}</div>
+		<div><button (click)="subscribeTimer1()">{{timer1button}} {{timer1Name}} timer</button>{{counter1}}</div>
+		<div><button (click)="subscribeTimer2()">{{timer2button}} {{timer2Name}} timer</button>{{counter2}}</div>
+		<div><button (click)="subscribeTimer3()">{{timer3button}} {{timer3Name}} timer</button>{{counter3}}</div>
 		<div><button (click)="delAllTimer()">Delete all timer</button></div>`
 })
 export class AppComponent implements OnInit {
@@ -17,32 +18,43 @@ export class AppComponent implements OnInit {
 
 	counter0 = 0;
 	timer0Id: string;
+	timer0Name: string = '1 sec'
 	timer0button = 'Subscribe';
 
 	counter1 = 0;
 	timer1Id: string;
+	timer1Name: string = '5 sec'
 	timer1button = 'Subscribe';
 
 	counter2 = 0;
 	timer2Id: string;
+	timer2Name: string = '10 sec'
 	timer2button = 'Subscribe';
+
+	counter3 = 0;
+	timer3Id: string;
+	timer3Name: string = '100 msec'
+	timer3button = 'Subscribe';
 
 	// Define SimpleTimer as 'st'
 	constructor(private st: SimpleTimer) { }
 
 	ngOnInit() {
-		this.st.newTimer('1sec', 1, true);
-		this.st.newTimer('5sec', 5, true);
-		this.st.newTimer('10sec', 10, true);
+		this.st.newTimer(this.timer0Name, 1, true);
+		this.st.newTimer(this.timer1Name, 5, true);
+		this.st.newTimerCD(this.timer2Name, 10, 10); // delay 10 sec
+		this.st.newTimerHR(this.timer3Name, 100, 100); // delay 0.1 sec
 		this.subscribeTimer0();
 		this.subscribeTimer1();
 		this.subscribeTimer2();
+		this.subscribeTimer3();
 	}
 
 	delAllTimer() {
-		this.st.delTimer('1sec');
-		this.st.delTimer('5sec');
-		this.st.delTimer('10sec');
+		this.st.delTimer(this.timer0Name);
+		this.st.delTimer(this.timer1Name);
+		this.st.delTimer(this.timer2Name);
+		this.st.delTimer(this.timer3Name);
 	}
 
 	subscribeTimer0() {
@@ -54,7 +66,7 @@ export class AppComponent implements OnInit {
 			console.log('timer 0 Unsubscribed.');
 		} else {
 			// Subscribe if timer Id is undefined
-			this.timer0Id = this.st.subscribe('1sec', () => this.timer0callback());
+			this.timer0Id = this.st.subscribe(this.timer0Name, () => this.timer0callback());
 			this.timer0button = 'Unsubscribe';
 			console.log('timer 0 Subscribed.');
 		}
@@ -70,7 +82,7 @@ export class AppComponent implements OnInit {
 			console.log('timer 1 Unsubscribed.');
 		} else {
 			// Subscribe if timer Id is undefined
-			this.timer1Id = this.st.subscribe('5sec', () => this.timer1callback());
+			this.timer1Id = this.st.subscribe(this.timer1Name, () => this.timer1callback());
 			this.timer1button = 'Unsubscribe';
 			console.log('timer 1 Subscribed.');
 		}
@@ -86,9 +98,25 @@ export class AppComponent implements OnInit {
 			console.log('timer 2 Unsubscribed.');
 		} else {
 			// Subscribe if timer Id is undefined
-			this.timer2Id = this.st.subscribe('10sec', () => this.timer2callback());
+			this.timer2Id = this.st.subscribe(this.timer2Name, () => this.timer2callback());
 			this.timer2button = 'Unsubscribe';
 			console.log('timer 2 Subscribed.');
+		}
+		console.log(this.st.getSubscription());
+	}
+
+	subscribeTimer3() {
+		if (this.timer3Id) {
+			// Unsubscribe if timer Id is defined
+			this.st.unsubscribe(this.timer3Id);
+			this.timer3Id = undefined;
+			this.timer3button = 'Subscribe';
+			console.log('timer 3 Unsubscribed.');
+		} else {
+			// Subscribe if timer Id is undefined
+			this.timer3Id = this.st.subscribe(this.timer3Name, () => this.timer3callback());
+			this.timer3button = 'Unsubscribe';
+			console.log('timer 3 Subscribed.');
 		}
 		console.log(this.st.getSubscription());
 	}
@@ -104,4 +132,9 @@ export class AppComponent implements OnInit {
 	timer2callback(): void {
 		this.counter2++;
 	}
+
+	timer3callback(): void {
+		this.counter3++;
+	}
+
 }
